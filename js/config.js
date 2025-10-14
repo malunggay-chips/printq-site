b2f3cc06f6dcb389c222f4849a0324d2810aa849
LadiesMan217 <jutayako666999@gmail.com>
2025-10-14 13:31:17 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // js/config.js
 export const SUPABASE_URL = "https://gtmchmkgjtsowgwrasye.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR_ANON_KEY_HERE"; // from Supabase API settings
-export const PAYMONGO_SECRET = "YOUR_PAYMONGO_TEST_SECRET_KEY"; // starts with sk_test_
-export const API_BASE = `${SUPABASE_URL}/functions/v1`; // for createPrint
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0bWNobWtnanRzb3dnd3Jhc3llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAyNzIwNjgsImV4cCI6MjA3NTg0ODA2OH0.DHjs_utI3w-edQlx70cSETjKdHYk6N6rUCG-B_wrEzo"; // from Supabase API settings
+export const PAYMONGO_SECRET = "sk_test_PzqVfX7X6SjuvJ7Fgm9hnNLu"; // starts with sk_test_
+export const API_BASE = `${https://gtmchmkgjtsowgwrasye.supabase.co}/functions/v1`; // for createPrint
