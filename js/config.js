938843c215e40140b0d298a0809dbe248568fe73
LadiesMan217 <jutayako666999@gmail.com>
2025-10-15 16:35:19 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // js/config.js
 export const SUPABASE_URL = "https://gtmchmkgjtsowgwrasye.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY"; // from Supabase > Settings > API
-export const PAYMONGO_SECRET = "sk_test_yourpaymongokey"; // Test key from PayMongo dashboard
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0bWNobWtnanRzb3dnd3Jhc3llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAyNzIwNjgsImV4cCI6MjA3NTg0ODA2OH0.DHjs_utI3w-edQlx70cSETjKdHYk6N6rUCG-B_wrEzo"; // from Supabase > Settings > API
+export const PAYMONGO_SECRET = "sk_test_PzqVfX7X6SjuvJ7Fgm9hnNLu"; // Test key from PayMongo dashboard
